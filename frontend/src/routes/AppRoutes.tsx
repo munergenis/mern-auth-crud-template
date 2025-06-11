5b688d567dd7d4726de8eb7d22f25baa965cfb4d
munergenis <munergenis@gmail.com>
2025-06-11 11:39:47 +0200

    feat: add Login and Register components with routing

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,17 +1,34 @@
-import { AppLayout } from '@/layouts/AppLayout';
+import { PrivateAppLayout } from '@/layouts/PrivateAppLayout';
+import { PublicAppLayout } from '@/layouts/PublicAppLayout';
 import { Home } from '@/pages/Home';
+import { Login } from '@/pages/Login';
+import { Register } from '@/pages/Register';
 import { Route, Routes } from 'react-router';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={<AppLayout />}
-      >
+      {/* PUBLIC ROUTES */}
+      <Route element={<PublicAppLayout />}>
         <Route
           index
           element={<Home />}
+        />
+        <Route
+          path="login"
+          element={<Login />}
+        />
+        <Route
+          path="register"
+          element={<Register />}
+        />
+      </Route>
+
+      {/* PRIVATE ROUTES */}
+      <Route element={<PrivateAppLayout />}>
+        <Route
+          path="user"
+          element={<div>Here goes private routes</div>}
         />
       </Route>
     </Routes>
