a76a28e7b88a049adeec120ba57b1ba6511ca5a9
munergenis <munergenis@gmail.com>
2025-06-11 11:23:42 +0200

    refactor: reubicate layouts

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
+import { AppLayout } from '@/layouts/AppLayout';
 import { Home } from '@/pages/Home';
-import { AppLayout } from '@/shared/layouts/AppLayout';
 import { Route, Routes } from 'react-router';
 
 const AppRoutes = () => {
