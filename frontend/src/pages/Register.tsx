ba780b71aaa4ddc61e0c8d68a6d45a0ba21db226
munergenis <munergenis@gmail.com>
2025-06-12 20:57:28 +0200

    feat-front: implement toast notifications on register

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -3,12 +3,14 @@ import type { RegisterUser } from '@/features/auth/interfaces/User';
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router';
 import { register } from '@/api/api';
+import { useEffect } from 'react';
+import { toast } from 'sonner';
 
 export const Register = () => {
   const navigate = useNavigate();
 
   const handleSubmit = (user: RegisterUser) => {
-    console.log(user);
+    signUp(user);
   };
 
   const {
@@ -16,11 +18,18 @@ export const Register = () => {
     isPending,
     isError,
   } = useMutation({
+    mutationKey: ['register'],
     mutationFn: register,
     onSuccess: () => {
       navigate('/dashboard', { replace: true });
     },
   });
+
+  useEffect(() => {
+    if (isError) {
+      toast.error('Error login');
+    }
+  }, [isError]);
 
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
